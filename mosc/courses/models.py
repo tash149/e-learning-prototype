6f070f1dc927acd18b7420a29910e73d98236406
Nedheesh Hasija <starknet@localhost.localdomain>
2018-10-14 23:45:24 +0530

    "Models have been changed to use inheritance and enable code reusability

--- mosc/courses/models.py
+++ mosc/courses/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
-
-from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -50,6 +51,45 @@ class Module(models.Model):
         Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return '{}. {}'.format(self.order,self.title)
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete='self')
+    content_type = models.ForeignKey(ContentType, limit_choices_to={'model_in':('text','video','image', 'file')},on_delete='self')
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete='self')
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.title
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
